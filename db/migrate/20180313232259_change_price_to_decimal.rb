38c2dd8e4143bf4825011a5095ad53abb8d57ae8
JESSIE STOCKS <jessika.not.lawrence@gmail.com>
2018-03-15 20:42:34 -0400

    created Supplier model, added 2 instances of Suppliers

--- db/migrate/20180313232259_change_price_to_decimal.rb
+++ db/migrate/20180313232259_change_price_to_decimal.rb
@@ -1,5 +1,5 @@
 class ChangePriceToDecimal < ActiveRecord::Migration[5.1]
   def change
-    change_column :createproducts, :price, :decimal, precision: 5, scale: 2
+    change_column :products, :price, :decimal, precision: 5, scale: 2
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
 Product.create({name: "asdkjf;jk", price: 2, ...}) #.create does the creating and the saving so you can get lost on what went wrong, also a break doesn't provide and error; it fails silently
+
+product1 = Product.new({name: "Thingamabob", price: 1, image_url: "https://images-na.ssl-images-amazon.com/images/I/A1d5U2BMH1L.jpg", description: "I don't even know"})
+
+product1.save
+
+product2 = Product.new({name: "Doohickey", price: 2, image_url: "https://i.ytimg.com/vi/KIvDoReLbBo/hqdefault.jpg", description: "second product"})
+
+product2.save
