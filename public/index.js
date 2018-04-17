73c83103fc08f86144ca23dae4221ca5dab44929
JESSIE STOCKS <jessika.not.lawrence@gmail.com>
2018-04-16 20:25:31 -0400

    edited template classes & routes, added router-view tags

--- public/index.js
+++ public/index.js
@@ -23,18 +23,18 @@
 // });
 // }
 
-setTimeout(function() {
-  console.log("Second task done!");
-}, 4000)
+// setTimeout(function() {
+//   console.log("Second task done!");
+// }, 4000)
 
-setTimeout(function() {
-  console.log("Third task done!");
+// setTimeout(function() {
+//   console.log("Third task done!");
 
-}, 6000)
+// }, 6000)
 
-setTimeout(function() {
-  console.log("First task done!");
-}, 2000)
+// setTimeout(function() {
+//   console.log("First task done!");
+// }, 2000)
 
 
 var HomePage = {
